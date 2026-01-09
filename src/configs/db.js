23658557aa9fc88bbfaf4660805f0d2f556c9c4b
tanakit <tanakit2003moonang@gmail.com>
2026-01-09 17:41:10 +0700

    uPdate

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,21 +1,21 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || 'admin',
-    password: process.env.DB_PASS || 'admin',
-    database: process.env.DB_NAME ||'toilet'
-})
-pool.getConnection((err, connection) => {
+    password: process.env.DB_PASSWORD || 'admin',
+    database: process.env.DB_NAME || 'toilet'
+});
+
+// เชื่อมต่อ database
+db.connect((err) => {
     if (err) {
         console.error('❌ Database connection failed:', err.message);
     } else {
         console.log('✅ Database connected successfully');
-        connection.release();
     }
 });
 
-module.exports = pool;
-module.exports = db
+module.exports = db;
